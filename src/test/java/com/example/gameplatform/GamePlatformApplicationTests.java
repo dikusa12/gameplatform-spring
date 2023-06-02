03b12a033e3a12690ae1bc609f6bfbcf1738867c
Evgeniy <kashnikow.ev@gmail.com>
2023-06-02 23:03:16 +0300

    initial

---
+++ src/test/java/com/example/gameplatform/GamePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gameplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
